6f9d8be4cc329fd063936e79eada2bb6fc8bbcce
reshavp07 <reshavp07@gmail.com>
2026-03-22 01:43:28 +0200

    Task 1,2,3 Completed

---
+++ Lecture 1/Task 3.py
@@ -0,0 +1,14 @@
+#Write a program that asks the user for the length and width of a rectangle. The program then prints out the perimeter and area of the rectangle. The perimeter of a rectangle is the sum of the lengths of all four sides?
+#ask the user for input
+#We will use float() so that user can enter decimals (like 2.7)
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+area = length * width
+perimeter = 2 * (length + width)
+print(f"\nResults:")
+print(f"Area: {area:.2f}")
+print(f"Perimeter: {perimeter}")
+
+
+
+
