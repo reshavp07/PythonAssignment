9ae283294a4b08200062cd2c82bbf18dbb9073aa
reshavp07 <reshavp07@gmail.com>
2026-03-24 18:47:45 +0200

    Task 1 completed

--- Lecture 1/Task 1.py
+++ Lecture 1/Task 1.py
@@ -1,4 +1,7 @@
 #Write a program that asks for your name and then greets you by your name?
 user_name = input("Enter your name: ")
-greeting = "Moi," + user_name + "!"
-print (greeting)
+print (f"Moi ,{user_name}!")
+
+
+
+
