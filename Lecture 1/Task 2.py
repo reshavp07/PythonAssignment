5a43955348cdadf3b162dcd88d9e6ef8e336948c
reshavp07 <reshavp07@gmail.com>
2026-03-26 04:09:58 +0200

    Task 1,2,3,4 and 6 completed

--- Lecture 1/Task 2.py
+++ Lecture 1/Task 2.py
@@ -2,6 +2,7 @@
 # Ask the user for the radius
 #We use float() because the radius could be decimal (like 5.2)
 #Calculate the area using formula Area = π(3.14/math.pi) * r^2
+
 import math
 
 radius = float(input("Enter the radius of the circle: "))
