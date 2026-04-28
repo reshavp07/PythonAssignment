d520b13cf3387b72cae52576623be9050875e6e3
Reshav <reshavp07@gmail.com>
2026-04-28 09:35:44 +0300

    Task Completed

--- Lecture 5/Task 1.py
+++ Lecture 5/Task 1.py
@@ -10,13 +10,13 @@ month = int(input("Enter the number of the month (1-12): "))
 
 
 if month == 12 or month == 1 or month == 2:
-    result = seasons               # Winter
+    result = seasons[0]              # Winter
 elif month >= 3 and month <= 5:
-    result = seasons               # Spring
+    result = seasons[1]               # Spring
 elif month >= 6 and month <= 8:
-    result = seasons               # Summer
+    result = seasons[2]               # Summer
 elif month >= 9 and month <= 11:
-    result = seasons               # Autumn
+    result = seasons[3]               # Autumn
 else:
     result = "Invalid month! Please enter a number between 1 and 12."
 
